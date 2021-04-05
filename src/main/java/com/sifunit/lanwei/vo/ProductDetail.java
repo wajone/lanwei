75036ed85c8e48353cb6b7b053a6090e68a2d8d6
wajone <kgboy21@163.com>
2021-04-05 21:07:34 +0800

    home 04052107

--- src/main/java/com/sifunit/lanwei/vo/ProductDetail.java
+++ src/main/java/com/sifunit/lanwei/vo/ProductDetail.java
@@ -1,8 +1,17 @@
 package com.sifunit.lanwei.vo;
 
+import lombok.Data;
+
+@Data
 public class ProductDetail {
 
     private long productId;
 
+    private String productName;
+
     private long productNum;
+
+    private String productColor;
+
+    private String productionDesc;
 }
